b6c4a8ab0605a3d98ae0f404e9300eb986be9e7b
yeahbutstill <mayadani28@gmail.com>
2022-08-15 13:36:59 +0700

    update and locking

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ sudo docker run --rm \
 -v "$PWD/storage/docker/mysqlpzn-db-data":/var/lib/mysql \
 mysql
 
+# Locking 
+- Locking adalah proses mengunci data di DBMS 
+- Proses mengunci data sangat penting dilakukan, salah satunya agar data benar-benar terjamin konsistensinya 
+- Karena pada kenyataannya, aplikasi yang akan kita buat pasti digunakan oleh banyak pengguna, dan banyak pengguna tersebut bisa saja akan mengakses data yang sama, jika tidak ada proses locking, bisa dipastikan akan terjadi RACE CONDITION, yaitu proses balapan ketika mengubah data yang sama 
+- Contoh saja, ketika kita belanja di toko online, kita akan balapan membeli barang yang sama, jika data tidak terjaga, bisa jadi kita salah mengupdate stock karena pada saat yang bersamaan banyak yang melakukan perubahan stock barang
+
+# Locking Record
+- Saat kita melakukan proses TRANSACTION, lalu kita melakukan prose perubahan data, data yang kita ubah tersebut akan secara otomatis di LOCK 
+- Hal ini membuat proses TRANSACTION sangat aman 
+- Oleh karena itu, sangat disarankan untuk selalu menggunakan fitur TRANSACTION ketika memanipulasi data di database, terutama ketika perintah manipulasinya lebih dari satu kali 
+- Locking ini akan membuat sebuah proses perubahan yang dilakukan oleh pihak lain akan diminta untuk menunggu 
+- Data akan di lock sampai kita melakukan COMMIT atau ROLLBACK transaksi tersebut
--- Test.sql
+++ Test.sql
@@ -132,179 +132,7 @@ VALUES ('P0010', 'Makanan', 'Bakso Rusuk', 20000, 300),
        ('P0016', 'Lain-Lain', 'Es Krim', 5000, 110),
        ('P0017', 'Makanan', 'Mie Ayam Jamur', 5000, 110),
        ('P0018', 'Makanan', 'Bakso Telur', 20000, 150),
-       ('P0018', 'Makanan', 'Bakso Jando', 25000, 300);
-SELECT * FROM products WHERE quantity > 100;
-SELECT * FROM products WHERE quantity >= 100;
-SELECT * FROM products WHERE category != 'Makanan';
-SELECT * FROM products WHERE category <> 'Minuman';
-SELECT * FROM products WHERE quantity > 100 AND price > 20000;
-SELECT * FROM products WHERE category = 'Makanan' AND price < 20000;
-
-SELECT * FROM products WHERE quantity > 100 OR price > 20000;
-
-# Kalau tidak pake tanda pioritas () defaultnya yang AND dulu di eksekusi
-SELECT id, name, price, quantity FROM products WHERE (category = 'Makanan' OR quantity > 500) AND price > 10000;
-
-# Kalau datanya sudah terlalu besar jangan gunakan LIKE
-SELECT * FROM products WHERE name LIKE '%mie%';
-
-SELECT * FROM products WHERE description IS NULL;
-SELECT * FROM products WHERE description IS NOT NULL;
-SELECT * FROM products WHERE price BETWEEN 10000 AND 20000;
-SELECT * FROM products WHERE price NOT BETWEEN 10000 AND 20000;
-
-SELECT * FROM products WHERE category IN ('Makanan', 'Minuman') ORDER BY category DESC;
-SELECT * FROM products WHERE category NOT IN ('Makanan', 'Minuman') ORDER BY category DESC;
-SELECT * FROM products WHERE category = 'Makanan' OR category = 'Minuman';
-
-SELECT * FROM products ORDER BY price ASC, id DESC;
-SELECT id, category, name FROM products ORDER BY category;
-
-SELECT * FROM products WHERE price > 0 ORDER BY price LIMIT 5;
-SELECT * FROM products ORDER BY price LIMIT 5;
-
-#page 1
-SELECT * FROM products WHERE quantity > 100 ORDER BY price LIMIT 2, 5;
-#page 2
-SELECT * FROM products WHERE quantity > 100 ORDER BY price LIMIT 0, 5;
-#page 3
-SELECT * FROM products WHERE quantity > 100 ORDER BY price LIMIT 10, 5;
-
-use `mysqlpzn-db`;
-show tables;
-CREATE TABLE barang (
-    id INT NOT NULL,
-    kode INT NOT NULL,
-    nama VARCHAR(100) NOT NULL,
-    harga INT NOT NULL DEFAULT 0,
-    jumlah INT NOT NULL DEFAULT 0
-) ENGINE = InnoDB;
-
-DESCRIBE barang;
-SHOW CREATE TABLE barang;
-
-DROP TABLE barang;
-
-ALTER TABLE barang ADD COLUMN nama TEXT;
-ALTER TABLE barang ADD COLUMN deskripsi TEXT;
-ALTER TABLE barang RENAME COLUMN nama TO nama_baru;
-ALTER TABLE barang MODIFY nama_baru VARCHAR(100) AFTER jumlah;
-ALTER TABLE barang MODIFY nama_baru VARCHAR(100) FIRST;
-ALTER TABLE barang DROP COLUMN nama_baru;
-ALTER TABLE barang MODIFY id INT NOT NULL;
-ALTER TABLE barang MODIFY kode INT NOT NULL;
-ALTER TABLE barang MODIFY nama VARCHAR(200) NOT NULL;
-
-ALTER TABLE barang modify jumlah INT NOT NULL DEFAULT 0;
-ALTER TABLE barang modify harga INT NOT NULL DEFAULT 0;
-ALTER TABLE barang add waktu_dibuat TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
-
-INSERT INTO barang (id, kode, nama, harga, jumlah) VALUES (1, 01, 'Apple Iphone 13', 15000000, 1000);
-SELECT * FROM barang;
-
-ALTER TABLE barang ADD PRIMARY KEY (id);
-
-TRUNCATE barang;
-DROP TABLE barang;
-
-CREATE TABLE products (
-    id VARCHAR(10) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    price INT UNSIGNED NOT NULL,
-    quantity INT UNSIGNED NOT NULL DEFAULT 0,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE = InnoDB;
-
-SHOW TABLES;
-DESCRIBE products;
-
-INSERT INTO products(id, name, price, quantity)
-VALUES ('P0001', 'Mie Ayam Original', 15000, 1000);
-
-INSERT INTO products(id, name, description, price, quantity)
-VALUES ('P0002', 'Mie Ayam Bakso Tahu', 'Mie Ayam Original + Bakso Tahu', 20000, 1000);
-
-INSERT INTO products(id, name, description, price, quantity)
-VALUES ('P0003', 'Mie Ayam Ceker', 'Mie Ayam Original + Ceker', 20000, 100),
-       ('P0004', 'Mie Ayam Special', 'Mie Ayam Original + Bakso Tahu + Ceker + Yamin', 25000, 100),
-       ('P0005', 'Mie Ayam Yamin', 'Mie Ayam Original + Yamin', 15000, 100),
-       ('P0006', 'Mie Ayam Yamin', 'Mie Ayam Yamin Original', 15000, 100),
-       ('P0007', 'Mie Ayam Yamin', 'Mie Ayam Cabe Rawit Original + Yamin', 15000, 100),
-       ('P0008', 'Mie Ayam Yamin', 'Mie Ayam Jumbo Original + Yamin', 15000, 100),
-       ('P0009', 'Mie Ayam Yamin', 'Mie Ayam Kids Original + Yamin', 15000, 100);
-
-INSERT INTO products(id, name, description, price, quantity)
-VALUES ('P0006', 'Mie Ayam Bakso Rudal', 'Mie Ayam Original + Bakso Rudal', 30000, 0);
-
-SELECT * FROM products;
-DROP TABLE products;
-
-SELECT id, name, price, quantity FROM products;
-SELECT name, quantity, id FROM products;
-
-CREATE TABLE products (
-    id VARCHAR(10) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    price INT UNSIGNED NOT NULL,
-    quantity INT UNSIGNED NOT NULL DEFAULT 0,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
-);
-
-DESCRIBE products;
-
-SELECT id, name, price, quantity FROM products WHERE quantity = 0;
-
-SELECT * FROM products WHERE id = 'P0001';
-
-ALTER TABLE products
-ADD COLUMN category ENUM('Makanan', 'Minuman', 'Lain-Lain')
-AFTER name;
-
-UPDATE products
-SET category = 'Makanan'
-WHERE id = 'P0001';
-
-UPDATE products SET description = 'Makan Makan Aja' WHERE id = 'P0001';
-UPDATE products SET category = 'Makanan', description = 'Mie Ayam + Ceker' WHERE id = 'P0003';
-UPDATE products SET price = price + 5000 WHERE id = 'P0004';
-SELECT * FROM products;
-
-DELETE FROM products WHERE id = 'P0009';
-
-# Alias untuk Kolom
-SELECT id as 'Kode',
-       name as 'Nama',
-       category as 'Kategori',
-       price as 'Harga',
-       quantity as 'Stok'
-FROM products;
-
-# Alias untuk Table
-SELECT p.id as 'Kode',
-       p.name as 'Nama',
-       p.category as 'Kategori',
-       p.price as 'Harga',
-       p.quantity as 'Stok'
-FROM products AS p;
-
-SELECT id, name, price, quantity
-FROM products
-WHERE quantity > 100;
-
-INSERT INTO products(id, category, name, price, quantity)
-VALUES ('P0010', 'Makanan', 'Bakso Rusuk', 20000, 300),
-       ('P0011', 'Minuman', 'Es Jeruk', 2500, 200),
-       ('P0012', 'Minuman', 'Es Campur', 5500, 100),
-       ('P0013', 'Minuman', 'Es Teh Manis', 5000, 300),
-       ('P0014', 'Lain-Lain', 'Kerupuk', 1500, 200),
-       ('P0015', 'Lain-Lain', 'Keripik Udang', 10000, 400),
-       ('P0016', 'Lain-Lain', 'Es Krim', 5000, 110),
-       ('P0017', 'Makanan', 'Mie Ayam Jamur', 5000, 110),
-       ('P0018', 'Makanan', 'Bakso Telur', 20000, 150),
-       ('P0018', 'Makanan', 'Bakso Jando', 25000, 300);
+       ('P0019', 'Makanan', 'Bakso Jando', 25000, 300);
 SELECT * FROM products WHERE quantity > 100;
 SELECT * FROM products WHERE quantity >= 100;
 SELECT * FROM products WHERE category != 'Makanan';
@@ -459,3 +287,216 @@ DESCRIBE productsV2;
 ALTER TABLE products ADD CONSTRAINT price_check1 CHECK ( price >= 1000 );
 ALTER TABLE products DROP CONSTRAINT price_check1;
 
+# Membuat Table dengan Index
+CREATE TABLE sellers (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    PRIMARY KEY (id),
+    UNIQUE KEY email_unique (email),
+    INDEX name_index (name)
+) ENGINE = InnoDB;
+
+# Menambah/Menghapus Index
+ALTER TABLE sellers ADD INDEX name_index(name);
+ALTER TABLE sellers DROP INDEX name_index;
+
+# Membuat Table dengan Full-Text Search
+CREATE TABLE productsfulltextsearch (
+    id VARCHAR(10) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price INT UNSIGNED NOT NULL,
+    quantity INT UNSIGNED NOT NULL DEFAULT 0,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FULLTEXT products_search (name, description)
+)ENGINE = InnoDB;
+
+DESCRIBE productsfulltextsearch;
+
+ALTER TABLE productsfulltextsearch ADD FULLTEXT products_search (name, description);
+ALTER TABLE productsfulltextsearch DROP INDEX products_search;
+
+INSERT INTO productsfulltextsearch(id, name, description, price, quantity)
+VALUES ('P0010', 'Bakso Rusuk', 'Bakso', 20000, 300),
+       ('P0011', 'Es Jeruk', 'Bakso',2500, 200),
+       ('P0012', 'Es Campur', 'Minuman ES', 5500, 100),
+       ('P0013', 'Es Teh Manis', 'Minuman ES', 5000, 300),
+       ('P0014', 'Kerupuk', 'Aneka Jajan', 1500, 200),
+       ('P0015', 'Keripik Udang', 'Aneka Jajan',10000, 400),
+       ('P0016', 'Es Krim', 'Minuman ES', 5000, 110),
+       ('P0017', 'Mie Ayam Jamur', 'Mie Ayam', 5000, 110),
+       ('P0018', 'Bakso Telur', 'Bakso', 20000, 150),
+       ('P0019', 'Bakso Jando', 'Bakso', 25000, 300),
+       ('P0020', 'Mie Ayam Bakso', 'Mie dan Bakso', 30000, 300),
+       ('P0021', 'Mie Ayam Bakso+Satekambing-emi', 'Mie dan Bakso', 30000, 300),
+       ('P0022', 'Mie Ayam Bakso Rusuk', 'Mie dan Bakso', 30000, 300);
+
+DROP TABLE productsfulltextsearch;
+
+# Mencari dengan Natural Language Mode
+SELECT * FROM productsfulltextsearch WHERE MATCH(name, description) AGAINST('ayam' IN NATURAL LANGUAGE MODE );
+
+#Mencari dengan Boolean Mode
+SELECT * FROM productsfulltextsearch WHERE MATCH(name, description) AGAINST('+mie -bakso' IN BOOLEAN MODE );
+
+# Mencari dengan Query Expansion Mode
+SELECT * FROM productsfulltextsearch WHERE MATCH(name, description) AGAINST('bakso' WITH QUERY EXPANSION );
+
+# Membuat Table dengan Foreign Key
+CREATE TABLE wishlist (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_product VARCHAR(10) NOT NULL,
+    description TEXT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_wishlist_product
+                      FOREIGN KEY (id_product) REFERENCES productsfulltextsearch (id)
+)ENGINE = InnoDB;
+
+DESCRIBE wishlist;
+
+ALTER TABLE wishlist DROP CONSTRAINT fk_wishlist_product;
+ALTER TABLE wishlist ADD CONSTRAINT fk_wishlist_product FOREIGN KEY (id_product) REFERENCES productsfulltextsearch (id);
+
+# Mengubah Behavior Menghapus Relasi
+ALTER TABLE wishlist ADD CONSTRAINT fk_wishlist_products FOREIGN KEY (id_product) REFERENCES productsfulltextsearch (id)
+ON DELETE  CASCADE ON UPDATE CASCADE;
+
+SELECT * FROM wishlist JOIN productsfulltextsearch ON (wishlist.id_product = productsfulltextsearch.id);
+
+SELECT productsfulltextsearch.id, productsfulltextsearch.name, wishlist.description FROM wishlist
+JOIN productsfulltextsearch ON (productsfulltextsearch.id = wishlist.id_product);
+
+# Membuat relasi ke table customer
+ALTER TABLE wishlist
+ADD COLUMN id_customer INT;
+
+ALTER TABLE wishlist
+ADD CONSTRAINT fk_wishlist_customer
+FOREIGN KEY (id_customer) REFERENCES customer(id);
+
+# Melakukan JOIN Multiple Table
+SELECT customer.email, productsfulltextsearch.id, productsfulltextsearch.name, wishlist.description
+FROM wishlist
+JOIN productsfulltextsearch ON (productsfulltextsearch.id = wishlist.id_product)
+JOIN customer ON (customer.id = wishlist.id_customer);
+
+# Membuat Table Wallet One To One
+CREATE TABLE wallet (
+    id INT NOT NULL AUTO_INCREMENT,
+    id_customer INT NOT NULL,
+    balance INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (id),
+    UNIQUE KEY fk_id_customer_unique (id_customer),
+    CONSTRAINT fk_wallet_customer
+                    FOREIGN KEY (id_customer) REFERENCES customer (id)
+) ENGINE = InnoDB;
+
+# Membuat Table Category One To Many
+CREATE TABLE categorie (
+    id VARCHAR(10) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+# Mengubah Table Product
+ALTER TABLE productsfulltextsearch DROP COLUMN category;
+ALTER TABLE productsfulltextsearch ADD COLUMN id_category VARCHAR(100);
+ALTER TABLE productsfulltextsearch ADD CONSTRAINT fk_product_category
+FOREIGN KEY (id_category) REFERENCES categorie (id);
+
+# Many to Many Relationship
+CREATE TABLE orders (
+    id INT NOT NULL AUTO_INCREMENT,
+    total INT NOT NULL,
+    order_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE orders_detail (
+    id_product VARCHAR(10) NOT NULL,
+    id_order INT NOT NULL,
+    price INT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (id_product, id_order)
+) ENGINE = InnoDB;
+
+# Membuat Foreign Key
+ALTER TABLE orders_detail
+ADD CONSTRAINT fk_orders_detail_product
+FOREIGN KEY (id_product) REFERENCES productsfulltextsearch (id);
+
+ALTER TABLE orders_detail
+ADD CONSTRAINT fk_orders_detail_order
+FOREIGN KEY (id_order) REFERENCES orders (id);
+
+# Melihat Data Order, Detail dan Product-nya
+SELECT * FROM orders JOIN orders_detail ON (orders_detail.id_order = orders.id)
+JOIN productsfulltextsearch ON (productsfulltextsearch.id = orders_detail.id_product);
+
+# Melakukan Inner Join
+SELECT * FROM categorie INNER JOIN productsfulltextsearch ON (productsfulltextsearch.id = categorie.id);
+
+# Melakukan Left Join
+SELECT * FROM categorie LEFT JOIN productsfulltextsearch ON (productsfulltextsearch.id_category = categorie.id);
+
+# Melakukan Right Join
+SELECT * FROM categorie RIGHT JOIN productsfulltextsearch ON (productsfulltextsearch.id_category = categorie.id);
+
+# Menggunakan Cross Join
+SELECT * FROM categorie CROSS JOIN productsfulltextsearch;
+
+# Membuat Table Perkalian
+CREATE TABLE numbers (
+    id INT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+# Cross Join Table Perkalian
+SELECT numbers1.id, numbers2.id, (numbers1,id * numbers2.id) AS result FROM numbers AS numbers1
+CROSS JOIN numbers AS numbers2
+ORDER BY numbers1.id, numbers2.id;
+
+# Melakukan Subquery di WHERE Clause
+SELECT * FROM productsfulltextsearch WHERE price > (SELECT AVG(price) FROM productsfulltextsearch);
+
+# Melakukan Subquery di FROM Clause
+SELECT MAX(price)
+FROM (SELECT price FROM categorie INNER JOIN productsfulltextsearch ON (productsfulltextsearch.id_category = categorie.id))
+AS cp;
+
+# Membuat Table Guest Book
+CREATE TABLE guestbooks (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    content TEXT,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+# Melakukan Query UNION
+SELECT DISTINCT email FROM customer UNION SELECT DISTINCT email FROM guestbooks;
+
+# Melakukan Query UNION ALL
+SELECT DISTINCT email FROM customer
+UNION ALL
+SELECT DISTINCT email FROM guestbooks;
+
+SELECT email, COUNT(email)
+FROM (SELECT DISTINCT email FROM customer
+UNION ALL
+SELECT DISTINCT email FROM guestbooks) AS emails
+GROUP BY email;
+
+# Melakukan Query INTERSECT
+SELECT DISTINCT email FROM customer
+WHERE email IN (SELECT DISTINCT email FROM guestbooks);
+
+SELECT DISTINCT customer.email FROM customer
+INNER JOIN guestbooks ON (guestbooks.email = customer.email);
+
+# Melakukan Query MINUS
+SELECT DISTINCT customer.email, guestbooks.email FROM customer
+LEFT JOIN guestbooks ON (guestbooks.email = customer.email)
+WHERE guestbooks.email IS NULL;
