9611c2dd8d84ae6d851e9176d6adb97994607257
yeahbutstill <mayadani28@gmail.com>
2022-10-02 22:39:55 +0700

    count read

--- database-notification/notfication.sql
+++ database-notification/notfication.sql
@@ -84,3 +84,51 @@ AND c.id = 'PROMO' ORDER BY n.created_at DESC;
 SELECT * FROM notifications
 n JOIN category c ON (n.category_id = c.id) WHERE (n.user_id = 'maya' OR n.user_id IS NULL)
 AND c.id = 'INFO' ORDER BY n.created_at DESC;
+
+# Notification Read
+CREATE TABLE notification_read (
+    id INT NOT NULL AUTO_INCREMENT,
+    is_read BOOLEAN NOT NULL,
+    notification_id INT NOT NULL,
+    user_id VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+ALTER TABLE notification_read
+ADD CONSTRAINT  fk_notification_read_notification
+FOREIGN KEY (notification_id) REFERENCES notifications(id);
+
+ALTER TABLE notification_read
+ADD CONSTRAINT fk_notification_read_user
+FOREIGN KEY (user_id) REFERENCES user(id);
+
+DESCRIBE notification_read;
+
+SELECT * FROM notifications;
+INSERT INTO notification_read(is_read, notification_id, user_id) VALUES (true, 2, 'dani');
+INSERT INTO notification_read(is_read, notification_id, user_id) VALUES (true, 2, 'maya');
+
+SELECT * FROM notifications
+n JOIN category c ON (n.category_id = c.id)
+LEFT JOIN notification_read nr ON (nr.notification_id = n.id)
+WHERE (n.user_id = 'dani' OR n.user_id IS NULL)
+AND (nr.user_id = 'dani' OR nr.user_id IS NULL)
+ORDER BY n.created_at DESC;
+
+INSERT INTO notifications(title, detail, category_id, user_id, created_at)
+VALUES ('Pesan lagi aja', 'AYo ayo jing pesan lagi', 'INFO', 'dani', CURRENT_TIMESTAMP());
+INSERT INTO notifications(title, detail, category_id, user_id, created_at)
+VALUES ('Pesan global lagi aja', 'AYo ayo jing globa pesanan lagi loh', 'PROMO', NULL, CURRENT_TIMESTAMP());
+
+# Counter
+SELECT COUNT(*) FROM notifications
+n JOIN category c ON (n.category_id = c.id)
+LEFT JOIN notification_read nr ON (nr.notification_id = n.id)
+WHERE (n.user_id = 'dani' OR n.user_id IS NULL)
+AND (nr.user_id IS NULL)
+ORDER BY n.created_at DESC;
+
+INSERT INTO notification_read(is_read, notification_id, user_id)
+VALUES (true, 4, 'dani');
+INSERT INTO notification_read(is_read, notification_id, user_id)
+VALUES (true, 5, 'dani');
